9a4bb2a946925ff355e325fae96d01824c5af428
Md.Redwan <150443639+Re1354@users.noreply.github.com>
2025-07-10 22:11:15 +0600

    1 commit
    Category, Product Page

--- src/main/java/com/example/campusCart/controller/DashboardController.java
+++ src/main/java/com/example/campusCart/controller/DashboardController.java
@@ -7,14 +7,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 
-
 @Controller
 public class DashboardController {
 
-     @GetMapping({"/","/dashboard/index"})
-    public String dashboardHome(Model model, Principal principal){
+    @GetMapping({"/", "/dashboard/index"})
+    public String dashboardHome(Model model, Principal principal) {
         return "dashboard/index";
-
     }
 
+    @GetMapping("/dashboard/add-category")
+    public String addCategoryPage(Model model) {
+        return "pages/addCategory"; // matches: resources/templates/pages/addCategory.html
+    }
+    @GetMapping("/dashboard/all-category")
+    public String allCategoryPage(Model model) {
+        return "pages/allCategory"; // matches: resources/templates/pages/allCategory.html
+    }
+    @GetMapping("/dashboard/add-product")
+    public String addProductPage(Model model) {
+    return "pages/addProduct";
+}
 }
