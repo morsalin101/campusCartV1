6571356b351751cbbd5d1fe5dcf9b069beddc9a4
Md.Morsalin <57835317+morsalin101@users.noreply.github.com>
2025-07-15 22:26:46 +0600

    sda

--- src/main/java/com/example/campusCart/controller/PublicController.java
+++ src/main/java/com/example/campusCart/controller/PublicController.java
@@ -1,12 +1,28 @@
 package com.example.campusCart.controller;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.security.core.Authentication;
+
+import java.util.Collection;
+
 @Controller
 public class PublicController {
 
-    @GetMapping("/")
-    public String index() {
-        return "index"; 
+ @RequestMapping(value= {"/default"}, method = RequestMethod.GET)
+    public String defaultAfterLogin() {
+        Collection<? extends GrantedAuthority> authorities;
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        authorities = auth.getAuthorities();
+        String myRole = authorities.toArray()[0].toString();
+        String admin = "ADMIN";
+        if (myRole.equals(admin)) {
+            return "redirect:/dashboard/index";
+        }
+        return "redirect:/";
     }
 }
