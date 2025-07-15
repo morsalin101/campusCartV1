83c4f97ca2f94281390257743d599dde3a646123
Md.Redwan <150443639+Re1354@users.noreply.github.com>
2025-07-16 00:23:56 +0600

    product

--- src/main/java/com/example/campusCart/controller/ProductController.java
+++ src/main/java/com/example/campusCart/controller/ProductController.java
@@ -1,5 +1,24 @@
 package com.example.campusCart.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@RequiredArgsConstructor
 public class ProductController {
-    
+
+    @GetMapping("/products")
+    public String addProductPage() {
+     return "dashboard/products";
+    }
+
+    @GetMapping("/dashboard/add_product")
+    public String showAddProductPage(Model model) {
+        return "dashboard/add_product";
+    }
+
+  
 }
